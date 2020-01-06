415420e1e2398391f52afbcc81a96b81c640de95
Tiago Porto <tiagoporto@ymail.com>
2020-01-05 21:37:30 -0300

    Add prettier settings and vscode recommended extensions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,31 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "@vue/standard",
+    "@vue/typescript",
+    "prettier",
+    "prettier/standard",
+    "prettier/vue"
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: "@typescript-eslint/parser"
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true
       }
     }
   ]
-}
+};
