d266c804ea9d457168cf57d3d8cef33b698ac440
Tiago Porto <tiagoporto@ymail.com>
2020-01-06 23:41:21 -0300

    Updates

---
+++ precache-manifest.93d6b412f917c9cc5911ea53d4d7d7bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d46dab23baa6cf498a",
+    "url": "/css/app.c715183a.css"
+  },
+  {
+    "revision": "974a3c8d1f9c12468517",
+    "url": "/css/create.eee8da10.css"
+  },
+  {
+    "revision": "1d785883eac779424eab7ba69dc93b44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d46dab23baa6cf498a",
+    "url": "/js/app.9e047c7c.js"
+  },
+  {
+    "revision": "31934fb8effab0b6b4eb",
+    "url": "/js/chunk-vendors.c79b41bf.js"
+  },
+  {
+    "revision": "974a3c8d1f9c12468517",
+    "url": "/js/create.fefcbaef.js"
+  },
+  {
+    "revision": "f59d3f471199f61f8d01",
+    "url": "/js/task.4420d848.js"
+  },
+  {
+    "revision": "7ae7ea815676d5f9826e0173c913161a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
