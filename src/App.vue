bc59bc27ad1607b5b46cd4e2cd9215116ca1255b
Tiago Porto <tiagoporto@ymail.com>
2020-01-09 01:01:28 -0300

    Fix route on ghpages

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ Settings.defaultLocale = 'us'
 
 export default Vue.extend({
   name: 'App'
+  beforeCreate() {
+    let path = localStorage.getItem('path')
+
+    if (path) {
+      localStorage.removeItem('path')
+      this.$router.push(path)
+    }
 })
 </script>
 
