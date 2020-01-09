2b83b31ca7d0b0f7b5cff8a29110ef888b48d61c
Tiago Porto <tiagoporto@ymail.com>
2020-01-09 02:56:01 -0300

    Add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  testMatch: ['<rootDir>/src/**/*.test.(js|jsx|ts|tsx)'],
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
 }
---
+++ src/utils/date.test.ts
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import { formatDate } from './date'
+
+describe('formatDate', () => {
+  it('formatDate', () => {
+    expect(formatDate('2020-01-05T23:25:07.551Z')).toBe('Jan 5, 2020')
+  })
+})
