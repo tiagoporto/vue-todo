58ebf17603503e337cb4456e73428d3396d6eaf6
Tiago Porto <tiagoporto@ymail.com>
2020-01-10 00:20:40 -0300

    Add mutation test

---
+++ stryker.conf.js
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+  config.set({
+    mutator: 'typescript',
+    packageManager: 'npm',
+    testRunner: 'jest',
+    transpilers: [],
+    coverageAnalysis: 'off',
+    mutate: ['src/utils/date.ts'],
+    babelrcFile: '.babelrc'
+  })
+}
