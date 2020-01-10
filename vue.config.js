b40eb04fa254fe9b8b476dfc7a9efb84b6a60745
Tiago Porto <tiagoporto@ymail.com>
2020-01-09 23:11:52 -0300

    Add icons

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,22 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-todo/' : '/',
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+    types.forEach(type =>
+      addStyleResource(config.module.rule('stylus').oneOf(type))
+    )
+  },
+  pwa: {
+    name: 'pawaTask',
+    themeColor: '#ffc038',
+    msTileColor: '#ffc038'
   }
 }
 
-function addStyleResource (rule) {
-  rule.use('style-resource')
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
     .loader('style-resources-loader')
     .options({
-      patterns: [
-        path.resolve(__dirname, './src/styles/settings.styl'),
-      ],
+      patterns: [path.resolve(__dirname, './src/styles/settings.styl')]
     })
 }
